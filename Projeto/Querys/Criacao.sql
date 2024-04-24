898d3b6d153fc636223cd49e85afd59e32a64fbd
William <willimarciano@hotmail.com>
2024-04-24 08:10:12 -0300

    Adcionando a query de criação do DB e Tabelas

---
+++ Projeto/Querys/Criacao.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE ThynkySys_Senac_GPA3;
+
+USE ThynkySys_Senac_GPA3;
+
+CREATE TABLE Pessoa_Vendedora(
+id_pessoaVendedora INT NOT NULL AUTO_INCREMENT,
+nomeUsuario VARCHAR(50),
+nomeLogin VARCHAR(50),
+senhaUsuario VARCHAR(50),
+enderecoUsuario VARCHAR(50),
+PRIMARY KEY (id_pessoaVendedora)
+);
+
+CREATE TABLE Pessoa_Usuario(
+id_pessoaUsuario INT NOT NULL AUTO_INCREMENT,
+nomeUsuario VARCHAR(50),
+nomeLogin VARCHAR(50),
+senhaUsuario VARCHAR(50),
+enderecoUsuario VARCHAR(50),
+PRIMARY KEY (id_pessoaUsuario)
+);
+
+CREATE TABLE Produto(
+id_produto INT NOT NULL AUTO_INCREMENT,
+nomeProduto VARCHAR(50),
+precoUnitario DOUBLE,
+PRIMARY KEY (id_produto)
+);
+
+CREATE TABLE Vendas(
+id_vendaProduto INT NOT NULL AUTO_INCREMENT,
+id_produto INT,
+valorTotal DOUBLE,
+PRIMARY KEY (id_vendaProduto),
+FOREIGN KEY (id_produto) REFERENCES Produto(id_produto)
+);
+
+CREATE TABLE Compras(
+id_compra INT NOT NULL AUTO_INCREMENT, 
+id_produto INT,
+id_pessoaUsuario INT,
+id_vendaProduto INT,
+PRIMARY KEY (id_compra),
+FOREIGN KEY (id_produto) REFERENCES Produto(id_produto),
+FOREIGN KEY (id_pessoaUsuario) REFERENCES Pessoa_Usuario(id_pessoaUsuario),
+FOREIGN KEY (id_vendaProduto) REFERENCES Vendas(id_vendaProduto)
+);
+

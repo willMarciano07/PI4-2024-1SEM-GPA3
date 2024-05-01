612a6e061f621640b43752cd3ca17176e72dadbe
William <willimarciano@hotmail.com>
2024-05-01 12:27:12 -0300

    Adcionando super classe Pessoas

---
+++ Projeto/BackEnd/pessoas/Pessoas.java
@@ -0,0 +1,41 @@
+package Projeto.BackEnd.pessoas;
+
+public class Pessoas {
+    String nomeUsuario;
+    String nomeLogin;
+    String senhaUsuario;
+    String enderecoUsuario;
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getNomeLogin() {
+        return nomeLogin;
+    }
+
+    public void setNomeLogin(String nomeLogin) {
+        this.nomeLogin = nomeLogin;
+    }
+
+    public String getSenhaUsuario() {
+        return senhaUsuario;
+    }
+
+    public void setSenhaUsuario(String senhaUsuario) {
+        this.senhaUsuario = senhaUsuario;
+    }
+
+    public String getEnderecoUsuario() {
+        return enderecoUsuario;
+    }
+
+    public void setEnderecoUsuario(String enderecoUsuario) {
+        this.enderecoUsuario = enderecoUsuario;
+    }
+}
+

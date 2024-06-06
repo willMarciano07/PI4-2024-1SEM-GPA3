c31dc1411bc6ece34743396150f95bea73de01f0
MatheusFracaroli <matheusfracaroli@hotmail.com>
2024-06-05 22:02:29 -0300

    Ajustando imagens do cardápio

--- Backend_PI4/pizza-fracaroli/src/main/java/com/pizzaria/fracaroli/FracaroliApplication.java
+++ Backend_PI4/pizza-fracaroli/src/main/java/com/pizzaria/fracaroli/FracaroliApplication.java
@@ -10,5 +10,3 @@ public class FracaroliApplication {
 		SpringApplication.run(FracaroliApplication.class, args);
 	}
 }
-
-
--- Backend_PI4/pizza-fracaroli/src/main/resources/db/migration/V1.0.2__criar-itens.sql
+++ Backend_PI4/pizza-fracaroli/src/main/resources/db/migration/V1.0.2__criar-itens.sql
@@ -1,9 +1,9 @@
 INSERT INTO `pizza-fracaroli`.`item`
 (`id`,`titulo`,`descricao`,`imagem`,`preco`,`categoria_id`)
 VALUES
-(1,'Pizza Margherita','A margherita ou marguerita é um tipo de pizza tradicional de Nápoles, na Itália','https://www.google.com/url?sa=i&url=https%3A%2F%2Freceitas.globo.com%2Fana-maria-braga%2Fsalgados%2Fpizza-margherita.ghtml&psig=AOvVaw2bpVRGE_7N4N5LBvgcLeiA&ust=1717717158549000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMD1sZzRxYYDFQAAAAAdAAAAABAE','R$ 47,50',1),
+(1,'Pizza Margherita','A margherita ou marguerita é um tipo de pizza tradicional de Nápoles, na Itália','https://s2-receitas.glbimg.com/aOqutzWVjENKMw1htEAzVs33CpE=/0x0:1200x675/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_1f540e0b94d8437dbbc39d567a1dee68/internal_photos/bs/2024/h/r/EfCbvqTbeDRAD3Lzc5xA/pizza-margherita.jpg','R$ 47,50',1),
 (2,'Pizza Frango c/ Catupiry','Deliciosa pizza coberta de frango temperado e catupiry original','https://th.bing.com/th/id/R.ca6cb4418411da47f29cf15069073112?rik=JcNrcKBG5GVPsg&pid=ImgRaw&r=0&sres=1&sresct=1','R$ 55,00',1),
-(3,'Pizza Calabresa','Pizza recheada com calabresa e cebola','https://www.google.com/url?sa=i&url=https%3A%2F%2Fthepracticalkitchen.com%2Fhomemade-nutella-pizza%2F&psig=AOvVaw2x_jSQOaOoeV1mccJPREpr&ust=1717717318470000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMib-ObRxYYDFQAAAAAdAAAAABAE','R$ 40,0',1),
+(3,'Pizza Calabresa','Pizza recheada com calabresa e cebola','https://www.sabornamesa.com.br/media/k2/items/cache/513d7a0ab11e38f7bd117d760146fed3_XL.jpg','R$ 40,0',1),
 (4,'Pizza Portuguesa','A tradicional pizza portuguesa sem queijo','https://th.bing.com/th/id/OIP.DYSK5oM0tmexddhKsh5MtQHaE6?rs=1&pid=ImgDetMain','R$ 45,50',1),
 (5,'Pizza Chocolate','Pizza com cobertura de chocolate ao leite','https://th.bing.com/th/id/OIP.O8c8A3Ygl396qT6oII1t7gHaHa?w=175&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7','R$ 42,00',2),
 (6,'Pizza Romeo e Julieta','A famosa combinação de queijo com goiabada','https://th.bing.com/th/id/OIP.pRNPnWz-fCcMmAN-cD2Y2QHaEK?w=297&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7','R$ 49,50',2),
@@ -11,6 +11,5 @@ VALUES
 (8,'Pizza Chocolate com morango','Pizza coberta com chocolate e morangos da estação','https://th.bing.com/th/id/OIP._lldxM2s6-OKC7jWxThznQHaEK?rs=1&pid=ImgDetMain','R$ 48,90',2),
 (9,'Coca-cola','O refrigetante mais famoso do mundo','https://choppmaisfacil.com.br/image/cache/catalog/produtos/Refrigerantes/1648036735_1SZ-1000x1000.jpg','R$ 15,50',3),
 (10,'Sprite','Delicioso refrigerante sabor limão, na Itália','https://superprix.vteximg.com.br/arquivos/ids/213242-600-600/548391_1.jpg?v=638187332100070000','R$ 12,00',3),
-(11,'Guaraná Antártica','O refrigerante mais famoso do Brasil','https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.extrabom.com.br%2Fp%2Frefrigerante-guarana-antarctica-garrafa-2l%2F4922%2F&psig=AOvVaw0yyXmnb3M6-HDjenIm3ct-&ust=1717717901229000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjHxIPUxYYDFQAAAAAdAAAAABAE','R$ 12,00',3),
-(12,'Fanta Laranja','O refrigerante mais amado pelo Kel','https://www.imigrantesbebidas.com.br/bebida/images/products/full/1883-refrigerante-fanta-laranja-2l.jpg','R$ 12,00',3)
-;
+(11,'Guaraná Antártica', 'O refrigerante mais famoso do Brasil','https://www.extrabom.com.br/uploads/produtos/350x350/4922_20230308144637_thumb_2L.png','R$ 12,00',3),
+(12,'Fanta Laranja','O refrigerante mais amado pelo Kel','https://www.imigrantesbebidas.com.br/bebida/images/products/full/1883-refrigerante-fanta-laranja-2l.jpg','R$ 12,00',3);
